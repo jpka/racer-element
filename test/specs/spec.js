971b64c3dc31d35cc5b98a1d02cbbc96ff725d75
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-04 19:26:12 -0300

    First

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,7 +1,44 @@
-describe("my-element", function() {  
-  var element;
+describe("racer-element", function() {
+  var element,
+  child,
+  modelData,
+  model = {
+    events: {},
+    get: function() {
+      return modelData;
+    },
+    on: function(name, path, cb) {
+      this.events[name] = cb;
+    },
+    trigger: function(name) {
+      this.events[name]();
+    }
+  };
 
   beforeEach(function() {
-    element = fixtures.window().document.createElement("my-element");
+    element = fixtures.window().document.createElement("jpka-racer-element");
+    child = fixtures.window().document.createElement("element-with-model");
+    modelData = {
+      text: "text"
+    };
+  });
+
+  it("should be able to set the element", function() {
+    element.child = child;
+    expect(element.webkitShadowRoot.querySelector("#child")).to.exist.and.to.deep.equal(child);
+  });
+
+  it("should update the child's model when a racer model is attached", function() {
+    element.child = child;
+    element.model = model;
+    expect(child.model).to.deep.equal(modelData);
+  });
+
+  it("should update the child's model when the racer model changes", function() {
+    element.child = child;
+    element.model = model;
+    modelData.text = "otherText";
+    model.trigger("change");
+    expect(child.model).to.deep.equal(modelData);
   });
 });
